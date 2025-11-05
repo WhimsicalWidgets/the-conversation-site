4da2e6b7cdd9f546a828fa31373ab01e0a283ac4
Kelly Johnson <MaitreyaBuddha@users.noreply.github.com>
2025-11-05 00:51:24 -0800

    add firebase reality

--- src/react-app/firebase.ts
+++ src/react-app/firebase.ts
@@ -2,15 +2,26 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBT7p8uCNSHWdZnipdDzMCHyvbXecdWxzQ",
+  authDomain: "the-conversation-site.firebaseapp.com",
+  projectId: "the-conversation-site",
+  storageBucket: "the-conversation-site.firebasestorage.app",
+  messagingSenderId: "1043752904679",
+  appId: "1:1043752904679:web:a4ed21f09121965c00ff42",
+  measurementId: "G-760DDQY24Y"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
